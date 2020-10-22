f0db58c67544b24bc94fdd5cbc7995cb5baac741
aronchen1 <chenar@mail.gvsu.edu>
2020-10-22 15:22:17 -0400

    fixed scene issue added camera

---
+++ docs/CIS350/Assets/Scripts/Grounded.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grounded : MonoBehaviour
+
+{
+    GameObject Naruto;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Naruto = gameObject.transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            Naruto.GetComponent<BasicMovement>().isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            Naruto.GetComponent<BasicMovement>().isGrounded = false;
+        }
+    }
+}
