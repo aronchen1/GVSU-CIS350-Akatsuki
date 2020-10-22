00447154501777024e27a08329ac2afccf95b55e
Ethan Chomoa <echomoa@gmail.com>
2020-10-22 19:11:39 -0400

    Added death mechanics

---
+++ docs/CIS350/Assets/Scripts/Death.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Death : MonoBehaviour
+{
+    public bool isDead;
+    public int minHeight = -20;
+    public int lives = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       isDead = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if( gameObject.transform.position.y < minHeight){
+            lives = lives - 1;
+            if (lives == 0){
+                lives = 3;
+            }
+            isDead = true;
+        }
+
+        if (isDead == true){
+            StartCoroutine ("Die");
+        }
+    }
+
+    IEnumerator Die (){
+        SceneManager.LoadScene ("Game");
+        SceneManager.LoadScene ("")
+         return null;
+    }
+}
