a739eca761d4955776d14bafe5c673c619896bdc
aronchen1 <chenar@mail.gvsu.edu>
2020-10-28 15:50:56 -0400

    adding project folder

---
+++ Projects/Assets/Scripts/Camera_Follow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Follow : MonoBehaviour
+{
+    public Transform playerNaruto;
+
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(playerNaruto.position.x, playerNaruto.position.y, transform.position.z);
+    }
+}
