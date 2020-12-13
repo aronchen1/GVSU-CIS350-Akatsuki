15be479f2f7f7e36bb83c7cefa250285d9d3e4c7
aronchen1 <chenar@mail.gvsu.edu>
2020-12-13 01:29:53 -0500

    added things for main menu

--- Projects/Assets/Scripts/PauseMenu.cs
+++ Projects/Assets/Scripts/PauseMenu.cs
@@ -41,7 +41,7 @@ public class PauseMenu : MonoBehaviour
 
     {
         Time.timeScale = 1f;
-        SceneManager.LoadScene("Menu");
+        SceneManager.LoadScene("MainMenu");
     }
 
     public void QuitGame()
