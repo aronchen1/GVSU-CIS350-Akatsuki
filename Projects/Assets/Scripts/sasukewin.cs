b7ad3b8da807c0fc50cd6f895088a8fa3595059d
Ethan Chomoa <echomoa@gmail.com>
2020-12-14 01:20:58 -0500

    did some stuff

---
+++ Projects/Assets/Scripts/sasukewin.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class sasukewin : MonoBehaviour
+{
+    public LayerMask enemyMask;
+   public float speed;
+   Rigidbody2D myBody;
+   Transform myTrans;
+    float myWidth;
+    float myHeight;
+    BasicMovement enemy;
+ 
+   void Start(){
+       myTrans = this.transform;
+       myBody = this.GetComponent<Rigidbody2D>();
+       SpriteRenderer mySprite = this.GetComponent<SpriteRenderer>();
+       myWidth = mySprite.bounds.extents.x;
+       myHeight = mySprite.bounds.extents.x;
+ 
+   }
+
+   void OnCollisionEnter2D(Collision2D collision){
+        BasicMovement enemy = collision.collider.GetComponent<BasicMovement>();
+        if(enemy != null){
+            SceneManager.LoadScene("End");
+        }
+    
+    }
+}
